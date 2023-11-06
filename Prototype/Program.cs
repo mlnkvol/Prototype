eb6bd71cf77f2dfddff27bb04801c0db2544613b
melni <melni@OLYA>
2023-11-06 10:49:31 +0200

    Add project files.

---
+++ Prototype/Program.cs
@@ -0,0 +1,93 @@
+﻿using PrototypeFigure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            IFigure figure = new Rectangle(10, 20);
+            IFigure clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+            figure = new Circle(15);
+            clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+            //My code
+            figure = new Triangle(3, 4, 5);
+            clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+            //My code
+            Console.Read();
+        }
+    }
+    interface IFigure
+    {
+        IFigure Clone();
+        void GetInfo();
+    }
+    class Rectangle : IFigure
+    {
+        int width;
+        int height;
+        public Rectangle(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+        public IFigure Clone()
+        {
+            return new Rectangle(this.width, this.height);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Прямокутник довжиною {0} и шириною {1}", height, width);
+        }
+    }
+    class Circle : IFigure
+    {
+        int radius;
+        public Circle(int r)
+        {
+            radius = r;
+        }
+        public IFigure Clone()
+        {
+            return new Circle(this.radius);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Круг радіусом {0}", radius);
+        }
+    }
+    //My code
+    class Triangle : IFigure
+    {
+        int side1;
+        int side2;
+        int side3;
+        public Triangle(int a, int b, int c)
+        {
+            side1 = a;
+            side2 = b;
+            side3 = c;
+        }
+        public IFigure Clone()
+        {
+            return new Triangle(this.side1, this.side2, this.side3);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Трикутник зі сторонами {0}, {1} та {2}", side1, side2, side3);
+        }
+    }
+    //My code
+}
